43f4798bf1f38beeef52d65f50742364e9b92c51
Nov11 <46375139+samiramiss11@users.noreply.github.com>
2019-01-27 18:51:04 +0100

    Create ConfigurationInfo.ps1

---
+++ ConfigurationInfo.ps1
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+#To get labilities
+Install-Module -Name Lability
+
+
+# these are path to configurations 
+Set-LabHostDefault -ConfigurationPath E:\Lability\Configurations
+Set-LabHostDefault -DifferencingVhdPath E:\Lability\VHD
+Set-LabHostDefault -HotfixPath E:\Lability\Hotfixes
+Set-LabHostDefault -ParentVhdPath E:\Lability\ParentVHD
+Set-LabHostDefault -IsoPath E:\Lability\ISO
+Set-LabHostDefault -ResourcePath E:\Lability\
